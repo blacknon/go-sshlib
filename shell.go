4f61c078b9c0151cc2c49e74780d14d4786d6e58
Blacknon <blacknon@orebibou.com>
2019-07-12 17:15:43 +0900

    test

--- shell.go
+++ shell.go
@@ -1,4 +1,68 @@
+// Copyright (c) 2019 Blacknon. All rights reserved.
+// Use of this source code is governed by an MIT license
+// that can be found in the LICENSE file.
+
 package sshlib
 
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"golang.org/x/crypto/ssh/terminal"
+)
+
 // Shell connect login shell over ssh.
-func (c Connect) Shell() {}
+//
+//
+func (c Connect) Shell() (err error) {
+	session, err := c.CreateSession()
+	if err != nil {
+		return
+	}
+
+	// input
+	fd := int(os.Stdin.Fd())
+	state, err := terminal.MakeRaw(fd)
+	if err != nil {
+		return
+	}
+	defer terminal.Restore(fd, state)
+
+	// Request tty
+	session, err = RequestTty(session)
+	if err != nil {
+		return
+	}
+
+	// Start shell
+	err = session.Shell()
+	if err != nil {
+		return
+	}
+
+	// Terminal resize
+	signal_chan := make(chan os.Signal, 1)
+	signal.Notify(signal_chan, syscall.SIGWINCH)
+	go func() {
+		for {
+			s := <-signal_chan
+			switch s {
+			case syscall.SIGWINCH:
+				fd := int(os.Stdout.Fd())
+				width, height, _ = terminal.GetSize(fd)
+				session.WindowChange(height, width)
+			}
+		}
+	}()
+
+	// keep alive packet
+	go c.SendKeepAlive(session)
+
+	err = session.Wait()
+	if err != nil {
+		return
+	}
+
+	return
+}
