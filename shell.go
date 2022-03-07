07db75bf8dbf2c58ee2b5761ccc3ebef806a1916
blacknon <blacknon@orebibou.com>
2022-03-07 14:10:09 +0900

    update.

--- shell.go
+++ shell.go
@@ -12,6 +12,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"runtime"
 	"time"
 
 	"github.com/lunixbochs/vtclean"
@@ -88,17 +89,30 @@ func (c *Connect) CmdShell(session *ssh.Session, command string) (err error) {
 
 func (c *Connect) setupShell(session *ssh.Session) (err error) {
 	// set FD
-	// if runtime.GOOS == "windows" {
-	// 	stdin, err := session.StdinPipe()
-	// 	if err != nil {
-	// 		return err
-	// 	}
-	// 	go io.Copy(stdin, os.Stdin)
-	// } else {
-	session.Stdin = os.Stdin
-	// }
-	session.Stdout = os.Stdout
-	session.Stderr = os.Stderr
+	if runtime.GOOS == "windows" {
+		stdin, err := session.StdinPipe()
+		if err != nil {
+			return err
+		}
+
+		stdout, err := session.StdoutPipe()
+		if err != nil {
+			return err
+		}
+
+		stderr, err := session.StderrPipe()
+		if err != nil {
+			return err
+		}
+
+		go io.Copy(stdin, os.Stdin)
+		go io.Copy(os.Stdout, stdout)
+		go io.Copy(os.Stderr, stderr)
+	} else {
+		session.Stdin = os.Stdin
+		session.Stdout = os.Stdout
+		session.Stderr = os.Stderr
+	}
 
 	// Logging
 	if c.logging {
