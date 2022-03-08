ddd45c69f7df63415b9cbab2474708344c96857a
Blacknon <blacknon@orebibou.com>
2022-03-08 22:16:59 +0900

    update.

--- shell_unix.go
+++ shell_unix.go
@@ -7,46 +7,10 @@
 package sshlib
 
 import (
-	"log"
+	"io"
 	"os"
-
-	"golang.org/x/crypto/ssh"
 )
 
-func (c *Connect) setupShell(session *ssh.Session) (err error) {
-	// set FD
-	session.Stdin = os.Stdin
-	session.Stdout = os.Stdout
-	session.Stderr = os.Stderr
-
-	// Logging
-	if c.logging {
-		err = c.logger(session)
-		if err != nil {
-			log.Println(err)
-		}
-	}
-	err = nil
-
-	// Request tty
-	err = RequestTty(session)
-	if err != nil {
-		return err
-	}
-
-	// x11 forwarding
-	if c.ForwardX11 {
-		err = c.X11Forward(session)
-		if err != nil {
-			log.Println(err)
-		}
-	}
-	err = nil
-
-	// ssh agent forwarding
-	if c.ForwardAgent {
-		c.ForwardSshAgent(session)
-	}
-
-	return
+func getStdin() io.ReadCloser {
+	return os.Stdin
 }
