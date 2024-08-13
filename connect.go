08fb85456c4820c74972acfa68a971d9b7f948cf
Blacknon <blacknon@orebibou.com>
2024-08-13 23:51:48 +0900

    update.

--- connect.go
+++ connect.go
@@ -117,15 +117,15 @@ func (c *Connect) CreateClient(host, port, user string, authMethods []ssh.AuthMe
 	uri := net.JoinHostPort(host, port)
 
 	timeout := 20
-	if c.ConnectTimeout > 0 {
-		timeout = c.ConnectTimeout
+	if c.ConnectTimeout == 0 {
+		c.ConnectTimeout = timeout
 	}
 
 	// Create new ssh.ClientConfig{}
 	config := &ssh.ClientConfig{
 		User:    user,
 		Auth:    authMethods,
-		Timeout: time.Duration(timeout) * time.Second,
+		Timeout: time.Duration(c.ConnectTimeout) * time.Second,
 	}
 
 	if c.HostKeyCallback != nil {
@@ -147,7 +147,7 @@ func (c *Connect) CreateClient(host, port, user string, authMethods []ssh.AuthMe
 		c.ProxyDialer = proxy.Direct
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeout)*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(c.ConnectTimeout)*time.Second)
 	defer cancel()
 
 	// Dial to host:port
@@ -157,7 +157,7 @@ func (c *Connect) CreateClient(host, port, user string, authMethods []ssh.AuthMe
 	}
 
 	// Set deadline
-	netConn.SetDeadline(time.Now().Add(time.Duration(timeout) * time.Second))
+	netConn.SetDeadline(time.Now().Add(time.Duration(c.ConnectTimeout) * time.Second))
 
 	// Create new ssh connect
 	sshCon, channel, req, cerr := ssh.NewClientConn(netConn, uri, config)
